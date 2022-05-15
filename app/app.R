9efb1a8012e6ed5b4fc3a8b8e0bcc9ef9fe1c27e
Maximilian Bernkopf <maximilian.bernkopf@gmail.com>
2022-05-15 17:34:02 +0200

    icon fix

--- app/app.R
+++ app/app.R
@@ -5,13 +5,14 @@
 
 library(shiny)
 library(shinydashboard)
+Sys.sleep(10)
 
 ui <- dashboardPage(
     dashboardHeader(title = "Basic dashboard"),
     ## Sidebar content
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+            menuItem("Dashboard", tabName = "dashboard", icon = icon("tachometer-alt")),
             menuItem("Widgets", tabName = "widgets", icon = icon("th"))
         )
     ),
@@ -22,14 +23,14 @@ ui <- dashboardPage(
             tabItem(tabName = "dashboard",
                     fluidRow(
                         box(plotOutput("plot1", height = 250)),
-                        
+
                         box(
                             title = "Controls",
                             sliderInput("slider", "Number of observations:", 1, 100, 50)
                         )
                     )
             ),
-            
+
             # Second tab content
             tabItem(tabName = "widgets",
                     h2("Widgets tab content")
@@ -41,7 +42,7 @@ ui <- dashboardPage(
 server <- function(input, output) {
     set.seed(122)
     histdata <- rnorm(500)
-    
+
     output$plot1 <- renderPlot({
         data <- histdata[seq_len(input$slider)]
         hist(data)
